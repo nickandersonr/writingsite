79b801e7ce8220429bc85d1aeaab4cac6b59a578
Nick Anderson <nanderson@bluemodus.com>
2014-07-25 16:12:38 -0600

    Deleted backups
    don't really need them anymore

--- backup2/>/> timeline.mm
+++
@@ -1,8 +0,0 @@
-<map version="0.8.1">
-<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1239592280682" ID="Freemind_Link_17890601" MODIFIED="1241988457069" TEXT="   Peter at Goodwill (Saturday June 14th Afternoon)">
-<node CREATED="1241493907788" ID="_" MODIFIED="1241988465973" POSITION="right" TEXT="Gardening Day (Sunday June 15th)">
-<node CREATED="1241493921834" ID="Freemind_Link_729387793" MODIFIED="1241988481113" TEXT="Robert&apos;s first appearance (Monday June 16th)"/>
-</node>
-</node>
-</map>
